4a3b9862792ff756460c2df1ee81c910796f1bb8
SKKUKang <153530459+SKKUKang@users.noreply.github.com>
2024-03-31 17:33:08 +0900

    Add files via upload
    2주차 과제입니다.

---
+++ async.js
@@ -0,0 +1,48 @@
+
+async function fetchUser() {
+
+  return 'ellie';
+}
+
+const user = fetchUser();
+user.then(console.log);
+console.log(user);
+
+// 2. await ✨
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getApple() {
+  await delay(2000);
+  return '🍎';
+}
+
+async function getBanana() {
+  await delay(1000);
+  return '🍌';
+}
+
+async function pickFruits() {
+  const applePromise = getApple();
+  const bananaPromise = getBanana();
+  const apple = await applePromise;
+  const banana = await bananaPromise;
+  return `${apple} + ${banana}`;
+}
+
+pickFruits().then(console.log);
+
+// 3. useful APIs ✨
+function pickAllFruits() {
+  return Promise.all([getApple(), getBanana()]).then(fruits =>
+    fruits.join(' + ')
+  );
+}
+pickAllFruits().then(console.log);
+
+function pickOnlyOne() {
+  return Promise.race([getApple(), getBanana()]);
+}
+
+pickOnlyOne().then(console.log);
---
+++ callback-to-promise.js
@@ -0,0 +1,30 @@
+// Callback Hell example
+class UserStorage {
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === 'ellie' && password === 'dream') ||
+          (id === 'coder' && password === 'academy')
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error('not found'));
+        }
+      }, 2000);
+    });
+  }
+
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === 'ellie') {
+          resolve({ name: 'ellie', role: 'admin' });
+        } else {
+          reject(new Error('no access'));
+        }
+      }, 1000);
+    });
+  }
+
+}
